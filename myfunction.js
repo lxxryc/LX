fd2cf22ec3b64096982a580a51f9e4572b80f79a
annonymous <lxx.ryc@gmail.com>
2025-09-11 00:43:08 +0800

    Update myfunction.js

--- myfunction.js
+++ myfunction.js
@@ -20,8 +20,8 @@
 
   const DURATION_SECONDS = 90; 
   const SECRET_MESSAGE = "In case you're starting to forget, know that you're loved in ways you might not always notice. It's not just in the words people say out loud, but in those secret glances, saved seats, and messages that check if you get home safely. You're the reason moments are lighter when you're in the room. Don't forget that sometimes, love reveals itself in the smallest, most precious details.";
-  const SELF_DESTRUCT_TEXT = "message destroyed! imissyou";
-  const AFTER_TEXT = "— 627";
+  const SELF_DESTRUCT_TEXT = "message destroyed";
+  const AFTER_TEXT = "— 62 7";
 
   const countdownTextEl = document.getElementById("countdownText");
   const countdownEl = document.getElementById("countdown");
